ebf06b0b3e37bbb21b727fe6c0ed243c8359cfcd
good edu <goodeducoop@gmail.com>
2017-06-29 22:50:21 +0900

    Initial commit

---
+++ app/src/main/java/com/nawoori/android/progress/MainActivity.java
@@ -0,0 +1,54 @@
+package com.nawoori.android.progress;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button, button2;
+    EditText editText;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputStr = editText.getText().toString().trim();
+                int input = Integer.parseInt(inputStr);
+
+                progressBar.setProgress(input);
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showProgressDialog();
+                
+            }
+        });
+    }
+
+            private void showProgressDialog() {
+                ProgressDialog dialog = new ProgressDialog(this);
+                dialog.setProgress(ProgressDialog.STYLE_SPINNER);
+                dialog.setMessage("데이터 확인중.");
+                dialog.show();
+
+
+    }
+    
+}
